2613ced6364ef846290c002643b965ca7f00a410
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-01-05 12:18:45 -0300

    feat: create header of home screen

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,27 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({theme}) => theme.colors.primaryBackground};
 `;
 
-export const Title = styled.Text`
-  font-family: ${({theme}) => theme.fonts.primary400};
-  font-size: 24px;
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: flex-end;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 32px 24px; 
+`;
+
+export const TotalCars = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary400};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.textDetail};
 `;
