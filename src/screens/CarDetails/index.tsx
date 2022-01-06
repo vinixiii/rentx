e840b13328619cf499b8c84b171a49c9eee832d0
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-01-06 10:22:18 -0300

    feat: create details and bout sections of car details screen

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
@@ -6,10 +6,19 @@ import { ImageSlider } from '../../components/ImageSlider';
 import {
   Container,
   Header,
-  CarImages
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About
 } from './styles';
 
-export function CarDetails() {
+export function CarDetails() {  
   return(
     <Container>
       <Header>
@@ -17,8 +26,27 @@ export function CarDetails() {
       </Header>
       
       <CarImages>
-        <ImageSlider imagesUrl={['https://cdn.picpng.com/porsche/porsche-view-29291.png']} />
+        <ImageSlider imagesUrl={['https://www.seekpng.com/png/full/256-2561343_lamborghini-huracan.png']} />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huaracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>{`R$ 580`}</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. 
+          É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 };
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled(GestureHandlerRootView)`
   flex: 1;
@@ -18,4 +19,62 @@ export const Header = styled.View`
 
 export const CarImages = styled.View`
   margin-top: ${getStatusBarHeight() + 32}px;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    padding: 24,
+    alignItems: 'center',
+  },
+})`
+
+`;
+
+export const Details = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 38px;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary500};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.textDetail};
+  text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary500};
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const Rent = styled.View`
+
+`;
+
+export const Period = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary500};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.textDetail};
+  text-transform: uppercase;
+`;
+
+export const Price = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary500};
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.main};
+`;
+
+export const About = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary400};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.text};
+  text-align: justify;
+  margin-top: 24px;
 `;
