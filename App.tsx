740427d985d58865bc5d0c3d7b6359c9b1307146
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-01-07 08:58:17 -0300

    feat: create scheduling details screen

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import {
   Archivo_600SemiBold
 } from '@expo-google-fonts/archivo';
 
-import { Scheduling } from './src/screens/Scheduling';
+import { SchedulingDetails } from './src/screens/SchedulingDetails';
 
 import theme from './src/global/styles/theme';
 
@@ -33,7 +33,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling />
+      <SchedulingDetails />
     </ThemeProvider>
   );
 };
---
+++ src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import { useTheme } from 'styled-components';
+import { Feather } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  RentalPeriod,
+  CalendarIcon,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  RentalPrice,
+  RentalPriceTitle,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal,
+  Footer
+} from './styles';
+import theme from '../../global/styles/theme';
+
+export function SchedulingDetails() {
+  const theme = useTheme();
+
+  return(
+    <Container>
+      <Header>
+        <BackButton onPress={() => console.log('Teste')}/>
+      </Header>
+      
+      <CarImages>
+        <ImageSlider imagesUrl={['https://www.seekpng.com/png/full/256-2561343_lamborghini-huracan.png']} />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>{`R$ 580`}</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800hp" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
+
+        <RentalPeriod>
+          <CalendarIcon>
+            <Feather
+              name="calendar"
+              size={RFValue(24)}
+              color={theme.colors.secondaryBackground}
+            />
+          </CalendarIcon>
+
+          <DateInfo>
+            <DateTitle>De</DateTitle>
+            <DateValue>18/02/2022</DateValue>
+          </DateInfo>
+
+          <Feather
+            name="chevron-right"
+            size={RFValue(10)}
+            color={theme.colors.textDetail}
+          />
+
+          <DateInfo>
+            <DateTitle>Até</DateTitle>
+            <DateValue>18/02/2022</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+
+        <RentalPrice>
+          <RentalPriceTitle>Total</RentalPriceTitle>
+          <RentalPriceDetails>
+            <RentalPriceQuota>R$ 580 x 3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$ 2900</RentalPriceTotal>
+          </RentalPriceDetails>
+        </RentalPrice>
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+};
