71d8d133443130665dcfb2223b3de5cca67ae611
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-01-13 09:52:35 -0300

    feat: create bottom tab navigation

--- src/routes/app.stack.routes.tsx
+++ src/routes/app.stack.routes.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Splash } from '../screens/Splash';
-import { SignIn } from '../screens/SignIn';
-import { SignUpFirstStep } from '../screens/SignUp/SignUpFirstStep';
-import { SignUpSecondStep } from '../screens/SignUp/SignUpSecondStep';
 import { Home } from '../screens/Home';
 import { MyCars } from '../screens/MyCars';
 import { CarDetails } from '../screens/CarDetails';
@@ -14,40 +10,17 @@ import { Confirmation } from '../screens/Confirmation';
 
 const { Navigator, Screen } = createStackNavigator();
 
-export function StackRoutes() {
+export function AppStackRoutes() {
   return(
     <Navigator
       screenOptions={{
         headerShown: false,
       }}
-      initialRouteName="Home"
+      initialRouteName="StackHome"
     >
       <Screen
-        name="Splash"
-        component={Splash}
-      />
-      <Screen
-        name="SignIn"
-        component={SignIn}
-      />
-      <Screen
-        name="SignUpFirstStep"
-        component={SignUpFirstStep}
-      />
-      <Screen
-        name="SignUpSecondStep"
-        component={SignUpSecondStep}
-      />
-      <Screen
-        name="Home"
+        name="StackHome"
         component={Home}
-        options={{
-          gestureEnabled: false,
-        }}
-      />
-      <Screen
-        name="MyCars"
-        component={MyCars}
       />
       <Screen
         name="CarDetails"
--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -48,7 +48,7 @@ export function Splash() {
   });
 
   function startApp() {
-    navigation.navigate('Home');
+    navigation.navigate('SignIn');
   };
 
   useEffect(() => {
